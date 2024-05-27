0001e7fe808fa268dde10b031ffb7a58a22158fe
Paulo Vitor Freitas <paulovitor.freitas18@gmail.com>
2024-05-27 12:53:18 -0300

    Implementando as alterações necessárias para resolução do desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
-namespace DesafioPOO.Models
+namespace trilha_net_poo_desafio.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia");
+        }
+
+
+
     }
+
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,33 @@
-namespace DesafioPOO.Models
+namespace trilha_net_poo_desafio.Models
 {
-    public abstract class Smartphone
+
+public abstract class Smartphone
+{
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+    public string Numero { get; set; }
+    private string Modelo { get; set; }
+    private string IMEI { get; set; }
+    private int Memoria { get; set; }
+
+    public Smartphone(string numero, string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        Numero = numero;
+        Modelo = modelo;
+        IMEI = imei;
+        Memoria = memoria;
     }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
+}
 }
